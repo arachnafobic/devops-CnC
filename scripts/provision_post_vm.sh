0d7bbeaee4aab05bc126b5b72e2ffd49c510b679
Remco van Hest <remco@arachnafobic.org>
2017-07-27 16:28:24 +0200

    Updated post provision to include initial setup playbook runs

--- scripts/provision_post_vm.sh
+++ scripts/provision_post_vm.sh
@@ -60,13 +60,13 @@ source /opt/ansible/hacking/env-setup 1>/dev/null 2>&1
 
 mkdir -p /home/vagrant/.log
 
-#cd /home/vagrant/playbooks/ansible-vm
-#exe "Running ansible-vm playbook on vm" \
-#     bash -c 'ansible-playbook -v -i hosts site.yml 1>/home/vagrant/.log/ansible-vm.vm 2>&1'
-#
-#exe "Running ansible-vm playbook on CnC" \
-#     bash -c 'ansible-playbook -v -i hosts.local site.yml 1>/home/vagrant/.log/ansible-vm.cnc 2>&1'
-#
+cd /home/vagrant/ansible
+exe "Running initialize-vm playbook on vm-ubuntu" \
+     bash -c 'ansible-playbook -v -i inventories/hosts.vm playbooks/initialize-vms.yml 1>/home/vagrant/.log/ansible-vm.vm 2>&1'
+
+exe "Running initialize-vm playbook on CnC" \
+     bash -c 'ansible-playbook -v -i inventories/hosts.devops-CnC playbooks/initialize-vms.yml 1>/home/vagrant/.log/ansible-vm.cnc 2>&1'
+
 #cd /home/vagrant/playbooks/ansible-init
 #exe "Running ansible-init playbook on vm" \
 #     bash -c 'ansible-playbook -v -i hosts site.foo.yml 1>/home/vagrant/.log/ansible-init.vm 2>&1'
